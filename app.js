a364678f1de6d3111b230f8bf71d66ff327c8160
Matias Cabrejos <matias.cabrejos@hotmail.com>
2021-08-25 22:22:55 -0300

    Testing some things with express

--- app.js
+++ app.js
@@ -1,20 +1,38 @@
-const http = require('http');
+const http = require('http')
+const bodyParser = require('body-parser')
 
-const express = require('express');
+const express = require('express')
 
-const app = express();
+const app = express()
 
-app.use((req, res, next) => {
+app.use(express.urlencoded({ extended: false }))
+
+app.use('/add-product', (req, res, next) => {
+  res.send(
+    '<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+  )
+})
+
+app.post('/product', (req, res, next) => {
+  console.log(req.body.title)
+  res.redirect('/')
+})
+
+app.use('/', (req, res, next) => {
+  res.send('<h1>Hi there!</h1>')
+})
+
+/* app.use((req, res, next) => {
     console.log('First middleware!');
     next();  
 });
 
 app.use((req, res, next) => {
     console.log('Second middleware!');
-});
+}); */
 
-const server = http.createServer(app);
+const server = http.createServer(app)
 
-server.listen(3000);
+server.listen(3000)
 
-console.log('hi!');
+console.log('hi!')
